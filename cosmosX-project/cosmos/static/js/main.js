a7b3977397030fab2ea02ce1a82fbb18bede8b09
MortalZorchik <mr.zorchik@mail.ru>
2023-01-16 21:16:17 +1000

    maybeFinal
    commit

--- cosmosX-project/cosmos/static/js/main.js
+++ cosmosX-project/cosmos/static/js/main.js
@@ -17,3 +17,23 @@ const closeNav = () => {
     menuBtn.style.display = "inline-block";
 }
 closeBtn.addEventListener('click', closeNav)
+
+
+
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('element_show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.member_content');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
